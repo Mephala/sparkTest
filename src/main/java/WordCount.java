77e04423f79933ec102591f72f42b7616398f4ea
mephala <gokhan.ozgozen@gmail.com>
2017-03-26 13:15:17 +0300

    Large text file added.

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -11,34 +11,15 @@ import java.util.Arrays;
  */
 public class WordCount {
 
-    public static void wordCountJava8(String filename) {
-//        // Define a configuration to use to interact with Spark
-//        SparkConf conf = new SparkConf().setMaster("local").setAppName("Work Count App");
-//
-//        // Create a Java version of the Spark Context from the configuration
-//        JavaSparkContext sc = new JavaSparkContext(conf);
-//
-//        // Load the input data, which is a text file read from the command line
-//        JavaRDD<String> input = sc.textFile( filename );
-//
-//        // Java 8 with lambdas: split the input string into words
-//        JavaRDD<String> words = input.flatMap( s -> Arrays.asList( s.split( " " ) ) );
-//
-//        // Java 8 with lambdas: transform the collection of words into pairs (word and 1) and then count them
-//        JavaPairRDD<String, Integer> counts = words.mapToPair(t -> new Tuple2( t, 1 ) ).reduceByKey( (x, y) -> (int)x + (int)y );
-//
-//        // Save the word count back out to a text file, causing evaluation.
-//        counts.saveAsTextFile( "output" );
-    }
-
+    private static final String OUT_FOLDER = "/home/mephala/Desktop/out";
     public static void main(String[] args) {
         // Define a configuration to use to interact with Spark
         SparkConf conf = new SparkConf().setMaster("local").setAppName("Work Count App");
 
         // Create a Java version of the Spark Context from the configuration
         JavaSparkContext sc = new JavaSparkContext(conf);
-        String largeFilePath = "/home/mephala/Desktop/large.txt";
-        String outputFile = "/home/mephala/Desktop/out";
+        String largeFilePath = WordCount.class.getClassLoader().getResource("large.txt").getPath();
+        String outputFile = OUT_FOLDER;
 
 //        JavaRDD<String> textFile = sc.textFile("hdfs://...");
         JavaRDD<String> textFile = sc.textFile(largeFilePath);
